92ffad95a82e496d951020f66c0e4578e51e881d
Anup-maurya <9anup.maurya>
2023-06-10 23:30:29 +0530

    first commit

---
+++ Arrays/SetMatrixZeros.cpp
@@ -0,0 +1,41 @@
+//Set Matrix Zero
+/*Problem Statement: Given a matrix if an element in the matrix is 0 then you will have to set its entire column and row to 0 and then return the matrix.
+*/
+class Solution
+{
+
+public:
+  void setZeroes(vector<vector<int>>&matrix)
+  {
+    int m = matrix.size(), n = matrix[0].size();
+
+      vector<int>rows(m, 1),cols(n, 1);
+
+    for (int i=0;i<m;i++)
+      {
+	for (int j=0;j<n;j++)
+	  {
+	    if (matrix[i][j] == 0)
+	      {
+
+		rows[i]=0;
+
+		cols[j]=0;
+
+	      }
+	  }
+      }
+
+    for (int i=0;i<m;i++)
+      {
+	for (int j = 0; j < n; j++)
+	  {
+	    if (rows[i] == 0 || cols[j] == 0)
+	      {
+		matrix[i][j] = 0;
+	      }
+
+      }
+      }
+  }
+};
