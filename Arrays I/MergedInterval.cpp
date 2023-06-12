eb7ef8800eb65973838d8f3e6117b887423ac434
Anup-maurya <9anup.maurya>
2023-06-12 23:22:49 +0530

    Solved merge interval problem on arrays

---
+++ Arrays I/MergedInterval.cpp
@@ -0,0 +1,17 @@
+
+// 56. Merge Intervals
+// You are given N number of intervals, where each interval contains two integers denoting the start time and the end time for the interval.
+
+vector < vector < int >> merge(vector < vector < int >> & intervals) {
+  sort(intervals.begin(), intervals.end());
+ 
+  vector < vector < int >> merged;
+  for (auto interval: intervals) {
+    if (merged.empty() || merged.back()[1] < interval[0]) {
+      merged.push_back(interval);
+    } else {
+      merged.back()[1] = max(merged.back()[1], interval[1]);
+    }
+  }
+  return merged;
+}
