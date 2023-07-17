3a0103ff6ebaaf07b99d737b6c67d280b606928e
Anup Kumar Maurya <62827230+Anup-maurya@users.noreply.github.com>
2023-07-17 23:32:29 +0530

    CombinationSum.cpp

---
+++ Recursion /CombinationSum.cpp
@@ -0,0 +1,31 @@
+// 39. Combination Sum
+// Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+
+class Solution {
+    public:
+    void findCombination(int ind, int target, vector < int > & arr, vector < vector < int >> & ans, vector < int > & ds) {
+      if (ind == arr.size()) {
+        if (target == 0) {
+          ans.push_back(ds);
+        }
+        return;
+      }
+      // pick up the element 
+      if (arr[ind] <= target) {
+        ds.push_back(arr[ind]);
+        findCombination(ind, target - arr[ind], arr, ans, ds);
+        ds.pop_back();
+      }
+
+      findCombination(ind + 1, target, arr, ans, ds);
+
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+      vector < vector < int >> ans;
+      vector < int > ds;
+      findCombination(0, target, candidates, ans, ds);
+      return ans;
+    }
+};
